e9201d9ad3ccb24e40d62584128eafffd6f7c0e6
iamchioma <chiomalinaanaso@gmail.com>
2026-01-23 05:12:39 +0100

    set up prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,30 +1,30 @@
-import { useState, useEffect } from "react";
-import { Button } from "./components/ui/button";
+import { useState, useEffect } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    async function loadData() {
-      try {
-        const res = await fetch("/api/hello");
-        if (!res.ok) return;
+   useEffect(() => {
+      async function loadData() {
+         try {
+            const res = await fetch('/api/hello');
+            if (!res.ok) return;
 
-        const data = await res.text();
-        setMessage(data);
-      } catch (err) {
-        console.error(err);
+            const data = await res.text();
+            setMessage(data);
+         } catch (err) {
+            console.error(err);
+         }
       }
-    }
 
-    loadData();
-  }, []);
-  return (
-    <div className="ml-3">
-      <p className="p-8 bg-red-500 text-white text-5xl">{message}</p>
-      <Button>Button</Button>
-    </div>
-  );
+      loadData();
+   }, []);
+   return (
+      <div className="ml-3">
+         <p className="p-8 bg-red-500 text-white text-5xl">{message}</p>
+         <Button>Button</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,22 +1,22 @@
-import express from "express";
-import type { Response, Request } from "express";
-import dotenv from "dotenv"
+import express from 'express';
+import type { Response, Request } from 'express';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Anotate types in home route
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
 });
 
 // Anotate types in home route
-app.get("/api/hello", (req: Request, res: Response) => {
-    res.json({ message: "Hello World how are you!"});
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send('Hello Welcome to Summerizer!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
-})
+   console.log(`Server is running at http://localhost:${port}`);
+});
