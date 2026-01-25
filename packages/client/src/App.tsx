7b861e66b40d7c96a37073da80b677e7389e5d19
iamchioma <chiomalinaanaso@gmail.com>
2026-01-25 18:14:45 +0100

    build chatbot component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,28 +1,9 @@
-import { useState, useEffect } from 'react';
-import { Button } from './components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      async function loadData() {
-         try {
-            const res = await fetch('/api/hello');
-            if (!res.ok) return;
-
-            const data = await res.text();
-            setMessage(data);
-         } catch (err) {
-            console.error(err);
-         }
-      }
-
-      loadData();
-   }, []);
    return (
       <div className="ml-3">
-         <p className="p-8 bg-red-500 text-white text-5xl">{message}</p>
-         <Button>Button</Button>
+         <ChatBot />
       </div>
    );
 }
