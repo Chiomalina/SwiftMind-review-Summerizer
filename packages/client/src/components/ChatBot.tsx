0d25d9b6e0fcf2fa6ce87677d8af4b5b847da73a
iamchioma <chiomalinaanaso@gmail.com>
2026-01-28 13:11:02 +0100

    style rendered messages from prompt and chatbot

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -13,15 +13,20 @@ type ChatResponse = {
    message: string;
 };
 
+type Message = {
+   content: string;
+   role: 'user' | 'bot';
+};
+
 function ChatBot() {
-   const [messages, setMessages] = useState<string[]>([]);
+   const [messages, setMessages] = useState<Message[]>([]);
    const conversationId = useRef(crypto.randomUUID());
 
    // Destructure toolboxes to be used in useForm before accessing them.
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
    const onSubmit = async ({ prompt }: FormData) => {
-      setMessages((prev) => [...prev, prompt]);
+      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
       console.log(prompt);
 
       // reseting the text area to empty
@@ -31,7 +36,7 @@ function ChatBot() {
          prompt: prompt,
          conversationId: conversationId.current,
       });
-      setMessages((prev) => [...prev, data.message]);
+      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
    };
 
    const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
@@ -43,9 +48,19 @@ function ChatBot() {
 
    return (
       <div>
-         <div>
+         <div className="flex flex-col gap-4">
             {messages.map((message, index) => (
-               <p key={index}>{message}</p>
+               <p
+                  key={index}
+                  className={`px-3 py-1 rounded-xl 
+                     ${
+                        message.role === 'user'
+                           ? 'bg-blue-600 text-white self-end'
+                           : 'bg-gray-100 text-black self-start'
+                     }`}
+               >
+                  {message.content}
+               </p>
             ))}
          </div>
          <form
